daf52edd5d5ebabdf387ce092a3b1da19e8e59cc
nnrcschmdt <e.rico.schmidt@gmail.com>
2018-03-03 19:36:27 -0400

    añadida requieren_documentos_para_su_revisión

--- seguimiento/convocatorias/admin.py
+++ seguimiento/convocatorias/admin.py
@@ -6,7 +6,7 @@ from .models import Convocatoria
 
 @admin.register(Convocatoria)
 class ConvocatoriaAdmin(admin.ModelAdmin):
-    actions = ['no_necesitan_revisión']
+    actions = ['no_necesitan_revisión', 'requieren_documentos_para_su_revisión']
     list_display = ('objeto', 'entidad', 'añadida', 'estatus')
     list_filter = ('departamento', 'tipo', 'estatus')
     fieldsets = [
@@ -34,7 +34,12 @@ class ConvocatoriaAdmin(admin.ModelAdmin):
     def no_necesitan_revisión(self, request, queryset):
         queryset.update(estatus=2)
 
-    no_necesitan_revisión.short_description = "Marcar que las Convocatorias selecionadas no necesitan revisión"
+    no_necesitan_revisión.short_description = "Marcar que las Convocatorias seleccionadas no necesitan revisión"
+
+    def requieren_documentos_para_su_revisión(sel, request, queryset):
+        queryset.update(estatus=3)
+
+    requieren_documentos_para_su_revisión.short_description = "Marcar que las Convocatorias seleccionadas requieren documentos para su revisión"
 
     def enlace_infosicoes(self, obj):
         return format_html("<a href='{url}'>{url}</a>", url=obj.enlace)
