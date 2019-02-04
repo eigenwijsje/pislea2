ae490c27e391a45864cf8d1f8922182cf3a5984d
nnrcschmdt <e.rico.schmidt@gmail.com>
2019-02-03 21:32:58 -0400

    corregidas las rutas para el feed RSS

--- seguimiento/seguimiento/urls.py
+++ seguimiento/seguimiento/urls.py
@@ -12,13 +12,13 @@ admin.autodiscover()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('feed/', ÚltimasConvocatoriasFeed()),
+    path('convocatorias/feed', ÚltimasConvocatoriasFeed()),
     path('convocatorias/', ConvocatoriaListView.as_view(), name='convocatoria-list'),
     path('convocatorias/<slug:slug>', ConvocatoriaDetailView.as_view(), name='convocatoria-detail'),
     path('planes/', PlanListView.as_view(), name='plan-detail'),
     path('planes/<int:pk>', PlanDetailView.as_view(), name='plan-list'),
     path('fechas-pasadas', FechasPasadasView.as_view(), name='fechas-pasadas'),
-    path('proxima-fecha', PróximaFechaView.as_view(), name='prxima-fecha'),
+    path('proxima-fecha', PróximaFechaView.as_view(), name='proxima-fecha'),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('', HomepageView.as_view(), name="homepage"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
