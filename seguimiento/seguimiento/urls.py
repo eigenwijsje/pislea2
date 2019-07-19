899c657344a87b16a56af37916b3fbaa9226b6f3
Ernesto Rico Schmidt <e.rico.schmidt@gmail.com>
2019-07-19 17:32:04 -0400

    corregidos los nombres de las vistas de planes

--- seguimiento/seguimiento/urls.py
+++ seguimiento/seguimiento/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from convocatorias.views import ConvocatoriaDetailView, ConvocatoriaListView
 from convocatorias.feeds import ÚltimasConvocatoriasFeed
+from convocatorias.views import ConvocatoriaDetailView, ConvocatoriaListView
 from planes.views import PlanDetailView, PlanListView
 from .views import FechasPasadasView, HomepageView, PróximaFechaView
 
@@ -15,8 +15,8 @@ urlpatterns = [
     path('convocatorias/feed', ÚltimasConvocatoriasFeed()),
     path('convocatorias/', ConvocatoriaListView.as_view(), name='convocatoria-list'),
     path('convocatorias/<slug:slug>', ConvocatoriaDetailView.as_view(), name='convocatoria-detail'),
-    path('planes/', PlanListView.as_view(), name='plan-detail'),
-    path('planes/<int:pk>', PlanDetailView.as_view(), name='plan-list'),
+    path('planes/', PlanListView.as_view(), name='plan-list'),
+    path('planes/<int:pk>', PlanDetailView.as_view(), name='plan-detail'),
     path('fechas-pasadas', FechasPasadasView.as_view(), name='fechas-pasadas'),
     path('proxima-fecha', PróximaFechaView.as_view(), name='proxima-fecha'),
     path('pages/', include('django.contrib.flatpages.urls')),
