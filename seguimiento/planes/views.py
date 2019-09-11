a2191df275a483c4dce583cd310ffaedeec11778
Ernesto Rico Schmidt <e.rico.schmidt@gmail.com>
2019-09-11 17:40:26 -0400

    añadida la paginación y la descripción de la tarjeta de Twitter

--- seguimiento/planes/views.py
+++ seguimiento/planes/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import DetailView, ListView
-
+from django.conf import settings
 from .models import Plan
 
 
@@ -9,8 +9,24 @@ class PlanView(object):
 
 
 class PlanListView(PlanView, ListView):
-    pass
+    paginate_by = 10
 
 
 class PlanDetailView(PlanView, DetailView):
-    pass
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        plan = self.get_object()
+
+        context['card_title'] = 'Plan de Implementación de Software Libre y Estándares Abiertos'
+
+        if plan.recibido > settings.PLAZO_PLANES:
+            context['card_description'] = \
+                'El PISLEA de {entidad} fue recibido por la AGETIC con {días_de_retraso} y fue publicado {días_después}'.format(
+                    entidad=plan.entidad, días_de_retraso=plan.días_de_retraso, días_después=plan.días_después)
+        else:
+            context['card_description'] = \
+                'El PISLEA de {entidad} fue recibido por la AGETIC dentro del plazo y fue publicado {días_después}'.format(
+                    entidad=plan.entidad, días_después=plan.días_después)
+
+        return context
