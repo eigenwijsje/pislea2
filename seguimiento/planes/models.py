daaff0695b37f4a8c942b07bc05c5a648c220f86
Ernesto Rico Schmidt <e.rico.schmidt@gmail.com>
2019-09-11 17:37:55 -0400

    simplificado el modelo de Planes

--- seguimiento/planes/models.py
+++ seguimiento/planes/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
@@ -5,25 +6,25 @@ from django.urls import reverse
 
 class Plan(models.Model):
     ESTATUS_CHOICES = (
-        (1, "Nuevo"),
-        (2, "Plan requiere documento para su revisión"),
-        (3, "Documento requiere revisión"),
-        (4, "Documento en revisión"),
-        (5, "Documento ha sido revisado")
+        (1, "Documento requiere revisión"),
+        (2, "Documento ha sido revisado")
     )
-    entidad = models.CharField("Entidad", max_length=255)
-    fuente = models.URLField("fuente", max_length=255)
+    entidad = models.CharField("Entidad", max_length=255, unique=True)
+    fuente = models.URLField("Fuente", max_length=255)
     estatus = models.SmallIntegerField("Estatus", choices=ESTATUS_CHOICES, default=1)
-    documento = models.FileField("Documento", blank=True, upload_to='files')
-    publicado = models.DateField("Fecha de publicación")
-    revisado = models.DateTimeField("Fecha de revisión", blank=True, null=True)
+    documento = models.FileField("Documento", upload_to='planes')
+    recibido = models.DateField("Fecha de recepción", blank=True, null=True)
+    publicado = models.DateField("Fecha de publicación", blank=True, null=True)
+    revisado = models.DateField("Fecha de revisión", blank=True, null=True)
     revisor = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, related_name='planes',
                                 verbose_name="Revisor")
     comentarios = models.TextField("Comentarios de la revisión", blank=True, null=True)
+    páginas = models.IntegerField("Páginas en total", default=0)
+    páginas_quitadas = models.CharField("Páginas quitadas", blank=True, max_length=64, null=True)
 
     class Meta:
-        get_latest_by = 'publicado'
-        ordering = ['-publicado']
+        get_latest_by = 'recibido'
+        ordering = ['entidad']
         verbose_name = "Plan de Implementación"
         verbose_name_plural = "Planes de Implementación"
 
@@ -32,3 +33,14 @@ class Plan(models.Model):
 
     def get_absolute_url(self):
         return reverse('plan_detail', args=[str(self.pk)])
+
+    @property
+    def días_de_retraso(self):
+        if self.recibido > settings.PLAZO_PLANES:
+            return '{} días de retraso'.format((self.recibido - settings.PLAZO_PLANES).days)
+        else:
+            return None
+
+    @property
+    def días_después(self):
+        return '{} días después'.format((self.publicado - self.recibido).days)
