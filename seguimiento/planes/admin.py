94b3f1cedf68d31b38d43c56baee6bb2062c8834
Ernesto Rico Schmidt <e.rico.schmidt@gmail.com>
2019-09-11 17:39:11 -0400

    mejorada la interfaz de administración de Plan

--- seguimiento/planes/admin.py
+++ seguimiento/planes/admin.py
@@ -6,8 +6,20 @@ from .models import Plan
 
 @admin.register(Plan)
 class PlanAdmin(admin.ModelAdmin):
-    list_display = ('entidad', 'publicado', 'revisado', 'estatus')
+    fieldsets = (
+        (None, {
+            'fields': ('entidad', 'enlace_fuente', 'estatus', 'documento', 'comentarios')
+        }),
+        ('Fechas', {
+            'fields': ('recibido', 'publicado', 'revisado')
+        }),
+        ('Páginas', {
+            'fields': ('páginas', 'páginas_quitadas')
+        })
+    )
+    list_display = ('entidad', 'recibido', 'publicado')
     list_filter = ('estatus',)
+    readonly_fields = ['entidad', 'enlace_fuente', 'documento']
 
-    def enlace_origen(self, obj):
-        return format_html("<a href='{url}'>{url}</a>", url=obj.origen)
+    def enlace_fuente(self, obj):
+        return format_html("<a href='{url}'>{url}</a>", url=obj.fuente)
