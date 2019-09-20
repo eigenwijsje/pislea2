e567b415f8c3edd55757a9dbab6b76af2769d1cd
Ernesto Rico Schmidt <e.rico.schmidt@gmail.com>
2019-09-20 18:28:56 -0400

    añadido el viewset de convocatorias

--- seguimiento/convocatorias/views.py
+++ seguimiento/convocatorias/views.py
@@ -1,12 +1,12 @@
 from django.views.generic import DetailView, ListView
-from django.db.models import Q
+from rest_framework import viewsets
 
 from .models import Convocatoria
+from .serializers import ConvocatoriaSerializer
 
 
 class ConvocatoriaView(object):
     model = Convocatoria
-    queryset = Convocatoria.objects.filter(Q(estatus=7) | Q(estatus=8))
 
 
 class ConvocatoriaListView(ConvocatoriaView, ListView):
@@ -22,4 +22,25 @@ class ConvocatoriaListView(ConvocatoriaView, ListView):
 
 
 class ConvocatoriaDetailView(ConvocatoriaView, DetailView):
-    pass
+    queryset = Convocatoria.objects.filter(estatus__in=[7, 8])
+
+
+class ConvocatoriaViewset(viewsets.ReadOnlyModelViewSet):
+    queryset = Convocatoria.objects.filter(estatus__in=[7, 8])
+    serializer_class = ConvocatoriaSerializer
+
+    def get_queryset(self):
+        queryset = Convocatoria.objects.filter(estatus__in=[7, 8])
+
+        entidad = self.request.query_params.get('entidad', None)
+        estatus = self.request.query_params.get('estatus', None)
+        departamento = self.request.query_params.get('departamento', None)
+
+        if entidad:
+            queryset = queryset.filter(entidad=entidad)
+        if estatus:
+            queryset = queryset.filter(estatus=estatus)
+        if departamento:
+            queryset = queryset.filter(departamento=departamento)
+
+        return queryset
